ab4bc8bf42c4f7001d618146e2a42ee379c4e1ba
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-06-02 23:09:52 +0300

    Update first_long_script.py

--- Python_scripts/first_long_script.py
+++ Python_scripts/first_long_script.py
@@ -37,3 +37,7 @@ while (line := f.readline()) != '':
 
 # Fibonacci algorithm
 fib = lambda x: x if x<=1 else fib(x-1) + fib(x-2)
+
+# quick sort, a famous sorting algorithm
+q = lambda l: q([x for x in l[1:] if x <= l[0]]) + [l[0]] + q([x for x in l if x > l[0]]) if l else []
+q([])
