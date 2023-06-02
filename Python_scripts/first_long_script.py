c357c11ed1c5b92fae515b69c61ab195bf028c04
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-06-02 23:04:17 +0300

    Update first_long_script.py

--- Python_scripts/first_long_script.py
+++ Python_scripts/first_long_script.py
@@ -2,3 +2,13 @@
 
 [line.strip() for line in open(filename)]
 
+map(lambda x:x**2, [i for i in range(3)]) 
+# <map object at 0x105558a90>
+
+filter(lambda x: x % 2 == 0, [1, 2, 3])
+# <filter object at 0x1056093d0>
+
+from functools import reduce
+reduce(lambda x, y: x + y, [1, 2, 3])
+# 6
+
