0dfa9572d1ac967c183cf9d761ffbb285f15d5ab
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-06-03 12:36:49 +0300

    Update first-long-script.js

--- JS_scripts/first-long-script.js
+++ JS_scripts/first-long-script.js
@@ -1,4 +1,48 @@
 /*
-First
+How to Capitalize Text
 */
+
+const capitalize = (str) => `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
+const name = "robert";
+capitalize(name) // "Robert";
+
+
+/*
+How to Calculate Percent
+*/
+
+const calculatePercent = (value, total) => Math.round((value / total) * 100)
+const questionsCorrect = 6;
+const questionTotal = 11;
+calculatePercent(questionsCorrect, questionsTotal); // 55
+
+
+/*
+How to Get a Random Element
+*/
+
+const getRandomItem = (items) =>  items[Math.floor(Math.random() * items.length)];
+const items = ["Nicely done!", "Good job!", "Good work!", "Correct!"];
+getRandomItem(items); // "Good job!"
+
+
+/*
+How to Remove Duplicate Elements
+*/
+
+const removeDuplicates = (arr) => [...new Set(arr)];
+const friendList = ["Jeff", "Jane", "Jane", "Rob"];
+removeDuplicates(friendList); // ['Jeff', 'Jane', 'Rob']
+
+
+/*
+How to Sort Elements By Certain Property
+*/
+
+const sortBy = (arr, key) => arr.sort((a, b) => a[key] > b[key] ? 1 : a[key] < b[key] ? -1 : 0);
+const lessons = [{ position: 1, name: "Intro" }, { position: 0, name: "Basics" }];
+sortBy(lessons, 'position'); 
+// {position: 0, name: 'Basics'},
+// {position: 1, name: 'Intro'}
+
 
