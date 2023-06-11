54fa507d976a3571ec51654e652fd643be22f31a
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-06-11 18:45:55 +0300

    Update first_long_script.py

--- Python_scripts/first_long_script.py
+++ Python_scripts/first_long_script.py
@@ -7,7 +7,20 @@ print(a, b, c, d)
 # Shows: True 1 ["i", "j"] ok
 
 
-
+x = 3
+ 
+# Traditional way
+if x % 2 == 1:
+result = f"{x} is odd"
+else:
+    result = f"{x} is even"
+ 
+# Pythonic way
+result = f"{x} " + ("is odd" if x % 2 == 1 else "is even")
+ 
+# Result
+print(result)
+# Shows: 3 is odd
 
 
 # Найчастіший елемент у списку
