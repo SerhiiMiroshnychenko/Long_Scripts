ff77a634f1f6c4a37a79ab25f88125aacc0b0e5e
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-09-20 13:30:12 +0300

    Update first-long-script.js

--- JS_scripts/first-long-script.js
+++ JS_scripts/first-long-script.js
@@ -1,3 +1,16 @@
+// Default Parameters
+
+/*
+function test(a, b = 3, c = 42) {
+  return a + b + c;
+}
+console.log(test(5));
+*/
+
+// Full ES6 equivalent
+const test = (a, b = 3, c = 42) => a + b + c;
+console.log(test(5));
+
 // Functions
 
 function add(x, y) {
