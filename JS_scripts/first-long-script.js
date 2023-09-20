4fce35a12cffea323e73a53c017e48953fb7b56c
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-09-20 17:31:23 +0300

    Update first-long-script.js

--- JS_scripts/first-long-script.js
+++ JS_scripts/first-long-script.js
@@ -1,3 +1,40 @@
+// Iterators & Generators
+
+let myIterableObj = { 
+  [Symbol.iterator] : function* () {
+    yield 1; yield 2; yield 3;
+  }
+};
+console.log([...myIterableObj]); // [ 1, 2, 3 ]
+
+function* idMaker() {
+  let index = 0;
+  while (index < 5)
+    yield index++;
+}
+var gen = idMaker();
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+// Try to add one more console.log, just like the above see what happens.
+
+const arr = ['0', '1', '4', 'a', '9', 'c', '16'];
+const my_obj = {
+  [Symbol.iterator]: function*() {
+    for(let index of arr) {
+      yield `${index}`;
+    }
+  }
+};
+const all = [...my_obj] /* Here you can replace the '[...my_obj]' with 'arr'. */
+  .map(i => parseInt(i, 10))
+  .map(Math.sqrt)
+  .filter((i) => i < 5) /* try changing the value of 5 to 4 see what happens.*/
+  .reduce((i, d) => i + d); /* comment this line while you are changing the value of the line above */
+console.log(all);
+
 // Promises
 
 setTimeout(function() {
