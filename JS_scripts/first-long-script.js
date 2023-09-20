7ed006c98a10cea14f659b5eb0086c1a51edda2b
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-09-20 16:19:40 +0300

    Update first-long-script.js

--- JS_scripts/first-long-script.js
+++ JS_scripts/first-long-script.js
@@ -1,3 +1,26 @@
+// Array Destructuring
+
+let arr = ['1', '2', '3'];
+let [one, two, three] = arr;
+console.log(one); // 1
+console.log(two); // 2
+console.log(three); // 3
+
+let a = () => {
+    return [1, 3, 2];
+};
+let [one, , two] = a();
+console.log(one); // 1
+console.log(two); // 2
+
+let a, b, c = 4, d = 8;
+[a, b = 6] = [2];
+console.log(a); // 2
+console.log(b); // 6
+[c, d] = [d, c];
+console.log(c); // 8
+console.log(d); // 4
+
 // Object.assign()
 
 let person = {
