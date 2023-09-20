0a295199ab82687fef3d5889cd31f1d78775aadc
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-09-20 17:37:08 +0300

    Update first-long-script.js

--- JS_scripts/first-long-script.js
+++ JS_scripts/first-long-script.js
@@ -1,3 +1,34 @@
+// Build-in Methods
+
+let res = [4, 5, 1, 8, 2, 0].filter(function (x) {
+  return x > 3;
+})[0];
+console.log(res);
+
+let res =
+[4, 5, 1, 8, 2, 0].find(x => x > 3);
+console.log(res);
+
+let res =
+[4, 5, 1, 8, 2, 0].findIndex(x => x > 3);
+console.log(res);
+
+console.log(Array(3 + 1).join("foo")); //foofoofoo
+
+console.log("foo".repeat(3)); //foofoofoo
+
+console.log("SoloLearn".indexOf("Solo") === 0); // true
+console.log("SoloLearn".indexOf("Solo") === (4 - "Solo".length)); // true
+console.log("SoloLearn".indexOf("loLe") !== -1); // true
+console.log("SoloLearn".indexOf("olo", 1) !== -1); // true
+console.log("SoloLearn".indexOf("olo", 2) !== -1); // false
+
+console.log("SoloLearn".startsWith("Solo", 0)); // true
+console.log("SoloLearn".endsWith("Solo", 4)); // true
+console.log("SoloLearn".includes("loLe")); // true
+console.log("SoloLearn".includes("olo", 1)); // true
+console.log("SoloLearn".includes("olo", 2)); // false
+
 // Modules
 
 // lib/math.js
