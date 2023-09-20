cd40afdc6363ef0fd3bedab65a419471a245268a
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-09-20 16:33:08 +0300

    Update first-long-script.js

--- JS_scripts/first-long-script.js
+++ JS_scripts/first-long-script.js
@@ -1,3 +1,31 @@
+// Rest Parameters
+
+function containsAll(arr) {
+    for (let k = 1; k < arguments.length; k++) {
+        let num = arguments[k];
+        if (arr.indexOf(num) === -1) {
+            return false;
+        }
+    }
+    return true;
+}
+let x = [2, 4, 6, 7];
+console.log(containsAll(x, 2, 4, 7));
+console.log(containsAll(x, 6, 4, 9));
+
+function containsAll(arr, ...nums) {
+    for (let num of nums) {
+        if (arr.indexOf(num) === -1) {
+            return false;
+        }
+    }
+    return true;
+}
+let x = [2, 4, 6, 7];
+console.log(containsAll(x, 2, 4, 7));
+console.log(containsAll(x, 6, 4, 9));
+
+
 // Object Destructuring
 
 let obj = {h:100, s: true};
