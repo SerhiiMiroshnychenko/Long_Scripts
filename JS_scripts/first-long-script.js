45c68ace0d5aa973b017a0edbdbaa9e5b3750f1c
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-09-20 17:15:49 +0300

    Update first-long-script.js

--- JS_scripts/first-long-script.js
+++ JS_scripts/first-long-script.js
@@ -1,3 +1,15 @@
+// Map Object
+
+let map = new Map([['k1', 'v1'], ['k2', 'v2']]);
+console.log(map.size); // 2
+
+let map = new Map();
+map.set('k1', 'v1').set('k2', 'v2');
+console.log(map.get('k1')); // v1
+console.log(map.has('k2')); // true
+for (let kv of map.entries())
+    console.log(kv[0] + " : " + kv[1]);
+
 // Inheritance
 
 class Animal {
