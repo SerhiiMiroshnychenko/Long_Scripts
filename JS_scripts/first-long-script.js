8570569bf7bcd51b067ece991b5cfe4388c5a70f
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-09-20 13:54:23 +0300

    Update first-long-script.js

--- JS_scripts/first-long-script.js
+++ JS_scripts/first-long-script.js
@@ -1,3 +1,46 @@
+// Object.assign()
+
+let person = {
+    name: 'Jack',
+    age: 18,
+    sex: 'male'
+};
+let student = {
+    name: 'Bob',
+    age: 20,
+    xp: '2'
+};
+let newStudent = Object.assign({}, person, student);
+console.log(newStudent.name); // Bob
+console.log(newStudent.age); // 20
+console.log(newStudent.sex); // male
+console.log(newStudent.xp); // 2
+
+let person = {
+    name: 'Jack',
+    age: 18
+};
+let newPerson = person;
+newPerson.name = 'Bob';
+console.log(person.name); // Bob
+console.log(newPerson.name); // Bob
+
+let person = {
+    name: 'Jack',
+    age: 18
+};
+let newPerson = Object.assign({}, person);
+newPerson.name = 'Bob';
+console.log(person.name); // Jack
+console.log(newPerson.name); // Bob
+
+let person = {
+    name: 'Jack',
+    age: 18
+};
+let newPerson = Object.assign({}, person, {name: 'Bob'});
+console.log(newPerson.name); // Bob
+
 // Computed Property Names
 
 let prop = 'name';
