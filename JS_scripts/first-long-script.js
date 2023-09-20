2f5288e8d26df9abcbb42fd1e474c28a92e9e594
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-09-20 17:32:22 +0300

    Update first-long-script.js

--- JS_scripts/first-long-script.js
+++ JS_scripts/first-long-script.js
@@ -1,3 +1,14 @@
+// Modules
+
+// lib/math.js
+export let sum = (x, y) => { return x + y; }
+export let pi = 3.14;
+
+// app.js
+import * as math from "lib/math"
+console.log(`2p = + ${math.sum(math.pi, math.pi)}`)
+
+
 // Iterators & Generators
 
 let myIterableObj = { 
