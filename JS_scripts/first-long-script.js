a48e0bd02ea0947bffa99ae01a17e122436289e3
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-09-20 12:29:04 +0300

    Update first-long-script.js

--- JS_scripts/first-long-script.js
+++ JS_scripts/first-long-script.js
@@ -1,3 +1,27 @@
+// Var and Let difference
+
+function varTest() {
+    var x = 1;
+    if (true) {
+        var x = 2;  // same variable
+        console.log(x);  // 2
+    }
+    console.log(x);  // 2
+}
+
+function letTest() {
+    let x = 1;
+    if (true) {
+        let x = 2;  // different variable
+        console.log(x);  // 2
+    }
+    console.log(x);  // 1
+}
+
+varTest();
+letTest();
+
+
 // Generate Random Color
 const generateRandomHexColor = () =>
     `#${Math.floor(Math.random() * 0xffffff).toString(16)}`;
