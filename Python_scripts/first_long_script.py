16f3b3e40cefdfb8047cc4a8bccdb72a8804d693
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-10-29 13:31:15 +0200

    Update first_long_script.py
    Map + filter

--- Python_scripts/first_long_script.py
+++ Python_scripts/first_long_script.py
@@ -1,4 +1,22 @@
 """
+Map + filter
+"""
+
+import math
+
+def is_positive(num):
+     return num >= 0
+
+
+def sanitized_sqrt(numbers):
+     cleaned_iter = map(math.sqrt, filter(is_positive, numbers))
+     return list(cleaned_iter)
+
+
+print(sanitized_sqrt([25, 9, 81, -16, 0]))
+
+
+"""
 Starmap
 """
 
