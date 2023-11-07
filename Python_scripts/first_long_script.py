3a39ff109bde9760deb2b7e2db83caad7feb600b
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-11-07 09:58:10 +0200

    Update first_long_script.py
    Disemvowel trolls

--- Python_scripts/first_long_script.py
+++ Python_scripts/first_long_script.py
@@ -1,4 +1,22 @@
 """
+Disemvowel trolls
+"""
+
+def disemvowel_trolls(sentence: str) -> str:
+  return ''.join(c for c in sentence if c.lower() not in 'euioa')
+
+
+
+print(disemvowel_trolls("Delete all vowels!"))
+
+print(disemvowel_trolls("123@#$&+&()%©®™✓[]aety"))
+
+assert disemvowel_trolls("Delete all vowels!") == "Dlt ll vwls!"
+
+assert disemvowel_trolls("QWERTY") == "QWRTY"
+
+
+"""
 D trolls
 """
 
