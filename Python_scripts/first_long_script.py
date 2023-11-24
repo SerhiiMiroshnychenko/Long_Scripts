998187ca14d7e9b54da3a6ea350cd83531218457
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-11-24 09:19:03 +0200

    Update first_long_script.py
    Context manager

--- Python_scripts/first_long_script.py
+++ Python_scripts/first_long_script.py
@@ -1,4 +1,38 @@
 """
+Context manager
+"""
+
+class File(object):
+    def __init__(self, file_name, method):
+        self.file_obj = open(file_name, method)
+    def __enter__(self):
+        return self.file_obj
+    def __exit__(self, type, value, traceback):
+        self.file_obj.close()
+
+with File('demo.txt', 'w') as opened_file:
+    opened_file.write('Hola!')
+
+with File('demo.txt', 'r') as opened_file:
+    print(opened_file.read())
+
+
+class File(object):
+    def __init__(self, file_name, method):
+        self.file_obj = open(file_name, method)
+    def __enter__(self):
+        return self.file_obj
+    def __exit__(self, type, value, traceback):
+        print("Exception has been handled")
+        self.file_obj.close()
+        return True
+
+with File('demo.txt', 'w') as opened_file:
+    opened_file.undefined_function()
+
+
+
+"""
 Context manager as a generator
 """
 
