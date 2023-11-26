93ee66af0d2994d79b9f58de54691576dbc61ce5
Serhii Miroshnychenko <113379599+SerhiiMiroshnychenko@users.noreply.github.com>
2023-11-26 09:34:14 +0200

    Update first_long_script.py
    Class Decorator

--- Python_scripts/first_long_script.py
+++ Python_scripts/first_long_script.py
@@ -1,4 +1,36 @@
 """
+Class Decorator
+"""
+
+import functools
+
+class CountCalls:
+    def __init__(self, func):
+        functools.update_wrapper(self, func)
+        self.func = func
+        self.num_calls = 0
+
+    def show(self):
+      print(self.num_calls)
+      
+
+    def __call__(self, *args, **kwargs):
+        self.num_calls += 1
+        print(f"Call {self.num_calls} of {self.func.__name__!r}")
+        return self.func(*args, **kwargs)
+
+@CountCalls
+def say_whee():
+    print("Whee!")
+
+say_whee()
+say_whee()
+print(say_whee.num_calls)
+say_whee.show()
+
+
+
+"""
 Generator 
 """
 
